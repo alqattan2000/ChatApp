24042e49c745432ee7ec7e6917e1ada799668a87
alqattan2000 <alqattan2000@gmail.com>
2019-07-07 15:21:06 +0300

    'inital SocketIO'

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const socketIO = require('socket.io')
 const port = process.env.PORT
 
 const io = socketIO(server)
-
-io.on('connection',()=> {
+let count = 0
+io.on('connection',(socket)=> {
     console.log('New WebSocket Connection !!')
+    socket.emit('countUpdated',count)
+    socket.on('increment', ()=>{
+        count++
+        //socket.emit('countUpdated', count)
+        io.emit('countUpdated',count)
+    })
 })
 
 server.listen(port , () => {
