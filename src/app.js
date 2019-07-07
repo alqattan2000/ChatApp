e481128e8cd963e0c78b8c5b357710090b9c5ca0
alqattan2000 <alqattan2000@gmail.com>
2019-07-07 14:26:20 +0300

    init chat app

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+app.use(express.json())
+
+// Define path for Express Config
+
+const publicDirectoryPath = path.join(__dirname, '..', 'public')
+const viewPath = path.join(__dirname, '..', 'temp', 'views')
+const partialsPath = path.join(__dirname, '..', 'temp', 'partials')
+// setup handlebars engine & views locations
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to Serve 
+app.use(express.static(publicDirectoryPath))
+app.get('', (req, res) => {
+    res.render('index', {
+        name: 'Mohammed Qattan'
+    })
+})
+
+
+
+module.exports = app
