bab705953a62f39b8318b2bcaa8ff485abbd4929
alqattan2000 <alqattan2000@gmail.com>
2019-07-08 14:27:21 +0300

    Chat app start Ack

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ const port = process.env.PORT
 const io = socketIO(server)
 let count = 0
 io.on('connection',(socket)=> {
-    console.log('New WebSocket Connection !!')
-    socket.emit('countUpdated',count)
-    socket.on('increment', ()=>{
-        count++
-        //socket.emit('countUpdated', count)
-        io.emit('countUpdated',count)
+    socket.emit('message', 'Welcome!!')
+    socket.broadcast.emit('message', 'A New User Has Join')
+    // console.log('New WebSocket Connection !!')
+    // socket.emit('countUpdated',count)
+    // socket.on('increment', ()=>{
+    //     count++
+    //     //socket.emit('countUpdated', count)
+    //     io.emit('countUpdated',count)
+    // })
+    socket.on('sendLocation',({latitude,longitude})=>{
+        
+        io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`)
+    })
+    socket.on('SendMessage', (message, callback)=>{
+        console.log(message)
+        io.emit('broadcast',message)
+        callback()
+    })
+    socket.on('disconnect', ()=>{
+        io.emit('message' , 'User has lift !!')
     })
 })
-
 server.listen(port , () => {
     console.log('Server is up on port: ' + port)
 })
